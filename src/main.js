21bd551d311518687399354a700b48c511cc291a
Diamond <diamondfsd@gmail.com>
2017-01-06 10:10:35 +0800

    init pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import MarkdownEditor from './components/MarkdownEditor.vue'
-import MarkdownPreview from './components/MarkdownPreview'
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('MarkdownEditor', MarkdownEditor)
-  window.Vue.component('MarkdownPreview', MarkdownPreview)
-}
-
-module.exports = { MarkdownEditor, MarkdownPreview }
--- src/preivew.js
+++
@@ -1,7 +0,0 @@
-import MarkdownPreview from './components/MarkdownPreview'
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('MarkdownPreview', MarkdownPreview)
-}
-
-module.exports = MarkdownPreview
